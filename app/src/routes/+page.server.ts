4e7b483b346c9d3346cd16bb963acd8609dc3686
James Napier <jamesnapier64@gmail.com>
2023-11-26 21:03:40 +1300

    add feedback for bad code, add copy to clipboard for link

--- app/src/routes/+page.server.ts
+++ app/src/routes/+page.server.ts
@@ -1,6 +1,7 @@
 import type { Actions } from './$types';
 import { codeFormSchema, codeResSchema } from '$lib/zod';
 import { API_URL } from '$env/static/private';
+import { fail } from '@sveltejs/kit';
 
 export const actions = {
 	minimise: async ({ request }) => {
@@ -12,7 +13,11 @@ export const actions = {
 			method: 'POST',
 			body: JSON.stringify(code)
 		});
-		const resCode = codeResSchema.parse(await res.json());
-		return { success: true, code: resCode };
+		try {
+			const resCode = codeResSchema.parse(await res.json());
+			return { success: true, code: resCode };
+		} catch {
+			return fail(400, { incorrect: true });
+		}
 	}
 } satisfies Actions;
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { ActionData, SubmitFunction } from './$types';
 	import { enhance } from '$app/forms';
+	import toast, { Toaster } from 'svelte-french-toast';
 	export let form: ActionData;
 	let code = '';
 	let loading = false;
@@ -9,10 +10,22 @@
 		loading = true;
 		return async ({ result, update }) => {
 			loading = false;
+			if (result.type !== 'success') {
+				toast.error('Minimisation unsuccessful. Check you are entering valid Python 3.');
+			}
 			update({ reset: false });
 		};
 	};
+
+	const handleLinkCopy = () => {
+		if (!loading && form?.code?.link) {
+			navigator.clipboard.writeText(form.code.link);
+			toast.success('Copied link to clipboard');
+		}
+	};
 </script>
+
+<Toaster />
 
 <section class="flex flex-col text-center px-9">
 	<form method="POST" action="?/minimise" use:enhance={submitCode} class="flex flex-col">
@@ -22,9 +35,9 @@
 			class="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600"
 		>
 			<div class="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
-				<label for="comment" class="sr-only">Your comment</label>
+				<label for="code" class="sr-only">Your code</label>
 				<textarea
-					id="comment"
+					id="code"
 					rows="16"
 					class="w-full px-0 font-mono text-gray-900 bg-white border-0 dark:bg-gray-800 dark:text-white dark:placeholder-gray-400 focus:ring-offset-0 focus:ring-0 outline-none"
 					bind:value={code}
@@ -85,10 +98,71 @@
 		</div>
 	</form>
 	{#if form?.code && !loading}
-		<div class="w-full mb-4 border border-gray-200 rounded-lg dark:border-gray-600">
-			<label for="link" class="font-bold">Link:</label>
-			<input id="link" bind:value={form.code.link} readonly class=" w-96" />
-			<a href={form.code.link} target="_blank">Visit</a>
+		<div
+			class="flex flex-col w-full border border-gray-200 rounded-lg dark:border-gray-600 text-left p-6 gap-3"
+		>
+			<div class="flex justify-between">
+				<h3 class="text-2xl font-bold leading-none">Minimised code</h3>
+				<button
+					on:click={() => handleLinkCopy()}
+					type="button"
+					class="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500"
+				>
+					<svg
+						class="w-4 h-4"
+						aria-hidden="true"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="currentColor"
+						viewBox="0 0 18 18"
+					>
+						<path
+							d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z"
+						/>
+					</svg>
+					<span class="sr-only">Copy to clipboard</span>
+				</button>
+			</div>
+			<div class="flex gap-5">
+				<input
+					id="link"
+					bind:value={form.code.link}
+					on:focus={() => handleLinkCopy()}
+					readonly
+					class=" w-96"
+				/>
+				<a
+					href={form.code.link}
+					target="_blank"
+					class="font-medium text-blue-600 dark:text-blue-500 hover:underline inline-flex items-center"
+				>
+					<svg
+						class="w-[16px] h-[16px]"
+						aria-hidden="true"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 18 18"
+					>
+						<path
+							stroke="currentColor"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"
+						/>
+					</svg></a
+				>
+			</div>
+
+			<textarea
+				id="minimised_code"
+				class="w-full resize-none px-0 font-mono text-gray-900 bg-white border-0 dark:bg-gray-800 dark:text-white dark:placeholder-gray-400 focus:ring-offset-0 focus:ring-0 outline-none"
+				bind:value={form.code.code}
+				rows="16"
+				readonly
+				name="code"
+				placeholder="Your python code..."
+				required
+			></textarea>
 		</div>
 	{/if}
 </section>
