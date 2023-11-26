05ce76c9470c00dc658e515a4d569319247bee8f
James Napier <jamesnapier64@gmail.com>
2023-11-26 18:50:14 +1300

    added api url env variable

--- app/src/routes/+page.server.ts
+++ app/src/routes/+page.server.ts
@@ -1,10 +1,11 @@
 import type { Actions } from './$types';
 import { codeFormSchema, codeResSchema } from '$lib/zod';
+import { API_URL } from '$env/static/private';
 
 export const actions = {
 	minimise: async ({ request }) => {
 		const code = codeFormSchema.parse(await request.formData());
-		const res = await fetch('http://localhost:8000/minimise/link/', {
+		const res = await fetch(API_URL + 'minimise/link/', {
 			headers: {
 				'Content-Type': 'application/json'
 			},
